957915c22af660f8796608100c007518117f5c8f
Taump <taump@yandex.ru>
2021-12-31 19:01:17 +0700

    v0.0.15

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
+declare const ButtonTypes: ["default", "primary", "ghost", "dashed", "link", "text"];
+declare type ButtonType = typeof ButtonTypes[number];
+declare type SizeType = 'small' | 'middle' | 'large' | undefined;
 declare module 'obyte-qr-button' {
 
   import React from 'react';
-  import { ButtonProps } from 'antd/lib/Button';
 
   interface IConfig {
     title?: any;
@@ -12,6 +14,24 @@ declare module 'obyte-qr-button' {
     tooltipMobile?: string;
     install?: string;
     obyteIn?: string;
+  }
+
+  interface ButtonProps {
+    href: string;
+    target?: string;
+    onClick?: React.MouseEventHandler<HTMLElement>;
+    type: ButtonType;
+    size?: SizeType;
+    loading?: boolean;
+    prefixCls?: string;
+    className?: string;
+    ghost?: boolean;
+    danger?: boolean;
+    block?: boolean;
+    children?: React.ReactNode;
+    autoFocus?: boolean | undefined;
+    disabled?: boolean | undefined;
+    name?: string | undefined;
   }
 
   interface IQRButton extends ButtonProps {
